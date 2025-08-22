4480e83964faf285a3395378e3000aa6a9fbe5c1
arjun-00012 <153722491+arjun-00012@users.noreply.github.com>
2025-08-22 09:22:22 +0530

    msg

---
+++ students/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
+
+def home(request):
+    students = Student.objects.all()
+    return render(request, 'home.html', {'students': students})
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = StudentForm()
+    return render(request, 'add_student.html', {'form': form})
